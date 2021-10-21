39c0256760c358d78249ada3f22e469c96315a1b
Andres Ordoñez <andres_ordonezmo@fet.edu.co>
2021-10-21 16:14:21 -0500

    Creacion ruta 1 y 2

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
+});
+Route::get('/pagina1', function () {
+    return "'Hola Mundo'";
+    
+});
+Route::get('/pagina2', function () {
+    return "HOla Mundo 2";
+    
 });
